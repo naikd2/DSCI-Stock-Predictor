1696211803d6ab61d9412cb36a2f1fe043296667
dhruvit <naikd2@apps.tcnj.edu>
2021-04-14 20:37:41 -0400

    try reddit client

--- src/driver.py
+++ src/driver.py
@@ -1,5 +1,15 @@
-import requests
+import praw
 
+client_id = "enter-here"
+user_agent = f"linux:edu.drexel.data-science:v0.0.1 (by u/drexel_msds_stocks)"
+client_secret = "enter-here"
 
-r = requests.get("http://www.google.com")
-print(r)
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+)
+
+sub = reddit.subreddit("wallstreetbets")
+for post  in sub.search("AMD", time_filter="month"):
+    print(post.title)
